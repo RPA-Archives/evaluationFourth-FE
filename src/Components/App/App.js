697326af75e1a417ce7a60e8d34badb4b79c9457
Ram Prasad Agarwal <ramprasadagarwal313@gmail.com>
2018-02-28 02:39:33 +0530

    Header Component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from '../Header/header.js';
 
 class App extends Component {
   render() {
     return (
       <div>
-        Hello World
+        <Header />
       </div>
     );
   }
